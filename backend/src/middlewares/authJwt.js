d80936d880f06431a9043328ee494f591e763375
Mai <maidev@MacBook-Air-de-Mai.local>
2026-01-25 14:51:41 -0300

    fix slow

--- backend/src/middlewares/authJwt.js
+++ backend/src/middlewares/authJwt.js
@@ -1,12 +1,14 @@
-import { verifyToken } from '../utils/jwt.js';
-import User from '../models/userModel.js';
+import { verifyToken } from "../utils/jwt.js";
+import User from "../models/userModel.js";
 
 export const requireAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ error: "No autorizado - Token no proporcionado" });
+      return res
+        .status(401)
+        .json({ error: "No autorizado - Token no proporcionado" });
     }
 
     const token = authHeader.split(" ")[1];
@@ -17,6 +19,10 @@ export const requireAuth = async (req, res, next) => {
 
     req.user = user;
     req.userId = user._id;
+
+    //req.user = user;
+    //req.id = user._id; // Crea esta propiedad limpia
+   // next();
 
     next();
   } catch (error) {
