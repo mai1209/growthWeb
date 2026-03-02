22febf9425c6031c19edaa2ec6334e1453894cd3
Mai <maidev@Mac.fibertel.com.ar>
2026-03-02 18:09:09 -0300

    vercel fix tres

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:3000";
+//const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:3000";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: '',
 });
 
 // 1. INTERCEPTOR DE SOLICITUD (El que automatiza el Token)
