f1d071b9d60f1a3415afd53c9ffc38bbd808bc00
Mai1209 <maidev1209@gmail.com>
2026-05-29 22:54:10 -0300

    deploy path

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,4 @@
-import app from "../backend/server.js";
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const { default: app } = await import("../backend/server.js");
   return app(req, res);
 }
